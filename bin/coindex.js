e0d3f8a980dc9860b0cce31c75084b5f40a51ea6
Karan16-prog <karan.s1606@gmail.com>
2021-07-27 23:24:30 +0530

    Add files via upload

---
+++ bin/coindex.js
@@ -0,0 +1,9 @@
+#!/usr/bin/envnode
+const program = require('commander');
+const pkg = require('../package.json')
+program
+  .command('key', 'Manage API Key -- https://nomics.com ')
+  .parse(process.argv)
+//program.version('1.2.3').parse(process.argv);
+console.log('hello from coindex');
+//console.log(process.argv)
